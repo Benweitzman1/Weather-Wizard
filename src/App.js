5e0be5fbfa1948e0750937e5be08ab4419c0af7a
Ben Weitzman <benw@applydesign.io>
2023-08-22 15:18:18 +0300

    add more styles for buttons and texts

--- src/App.js
+++ src/App.js
@@ -48,7 +48,7 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <Router>
-        <Container>
+        <Container maxWidth="lg">
           <Header darkMode={darkMode} setDarkMode={setDarkMode} />
           <Routes>
             <Route path="/favorites"
--- src/containers/MainScreen.js
+++ src/containers/MainScreen.js
@@ -5,13 +5,19 @@ import { useDispatch, useSelector } from 'react-redux';
 import { addToFavorites, removeFromFavorites } from '../redux/slices/favoritesSlice';
 import { setWeatherForSelectedCity } from '../redux/slices/weatherSlice';
 import { Container, Typography, Button, Divider, Paper, Grid, Box } from '@mui/material';
+import { blue, grey } from '@mui/material/colors';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import { useTheme } from '@mui/material/styles';
 
 function MainScreen({ setSnackbarOpen, setSnackbarMessage }) {
   const weatherData = useSelector(state => state.weather);
-  const [city, setCity] = useState(weatherData.currentWeather[0] ? weatherData.currentWeather[0] : null);
-  const [isCelsius, setIsCelsius] = useState(true);
   const dispatch = useDispatch();
   const favorites = useSelector(state => state.favorites);
+  const theme = useTheme();
+  const [city, setCity] = useState(weatherData.currentWeather[0] ? weatherData.currentWeather[0] : null);
+  const [isCelsius, setIsCelsius] = useState(true);
+  const deafultCity = "Tel-Aviv"
 
   // Fetch initial weather data when component mounts or selected city changes
   useEffect(() => {
@@ -19,7 +25,7 @@ function MainScreen({ setSnackbarOpen, setSnackbarMessage }) {
       try {
         let targetCity = city;
           if (!city) {
-            targetCity = await searchCity("tel-aviv", setSnackbarOpen, setSnackbarMessage);
+            targetCity = await searchCity(deafultCity, setSnackbarOpen, setSnackbarMessage);
             targetCity = targetCity[0];
             setCity(targetCity);
           }
@@ -89,45 +95,70 @@ function MainScreen({ setSnackbarOpen, setSnackbarMessage }) {
   }
 
   return (
-    <Container>
+    <Container maxWidth="lg">
       <SearchBar setCity={setCity} />
-      {/* Render weather details if available; otherwise, show loading */}
       {city && weatherData.currentWeather && weatherData.currentWeather.length > 0 && weatherData.forecast ? (
-        <Paper elevation={3} style={{ padding: '20px', marginTop: '40px' }}>
+        <Paper elevation={15} style={{ padding: '20px', marginTop: '40px', backgroundColor: theme.palette.background.paper }}>
           <Grid container spacing={2} alignItems="center">
-            <Grid item xs={8}>
-              <Typography variant="h4">{city.LocalizedName}</Typography>
-              <Typography variant="h5">{getTemperature(weatherData.currentWeather[0].Temperature.Imperial.Value)}</Typography>
-              <Typography variant="h5" style={{ textAlign: 'center', margin: '20px 0' }}>
-                {weatherData.currentWeather[0].WeatherText}
-              </Typography>
+            <Grid item xs={12} md={8} style={{ display: 'flex', alignItems: 'center', marginBottom: '65px' }}>
+              <div>
+                <Typography variant="h4" style={{ color: blue[800] }}>{city.LocalizedName}</Typography>
+                <Typography variant="h5" style={{ color: blue[600] }}>{getTemperature(weatherData.currentWeather[0].Temperature.Imperial.Value)}</Typography>
+                <Typography variant="h5" style={{ color: grey[600] }}>{weatherData.currentWeather[0].WeatherText}</Typography>
+              </div>
+              <Box display="flex" flexDirection="column" alignItems="center" ml={3}>
+                {isFavorite(city.Key) ? 
+                  <FavoriteIcon fontSize="large" style={{ color: blue[500] }} /> : 
+                  <FavoriteBorderIcon fontSize="large" style={{ color: blue[500] }} />
+                }
+                <Typography 
+                  variant="caption" 
+                  style={{ 
+                    color: isFavorite(city.Key) ? grey[700] : 'transparent',
+                    minHeight: '1rem'  // Ensure it takes up space even if transparent
+                  }}>
+                  In Favorites
+                </Typography>
+              </Box>
             </Grid>
-            <Grid item xs={4} style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-end' }}>
-              <div style={{ display: 'flex', alignItems: 'center', marginBottom: '50px' }}>
-                <Button
+            <Grid item xs={12} md={4} style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-end' }}>
+              <div style={{ width: '100%', marginBottom: '40px' }}>
+              <Button
                   onClick={toggleTemperatureScale}
                   variant="outlined"
                   color="primary"
-                  style={{ marginRight: '10px', minWidth: '180px', textTransform: 'none' }}
+                  fullWidth
+                  style={{ 
+                    marginRight: '10px', 
+                    textTransform: 'none', 
+                    background: 'linear-gradient(45deg, #2196F3 30%, #21CBF3 90%)',
+                    boxShadow: '0 3px 5px 2px rgba(33, 203, 243, .3)'
+                  }}
+                  onMouseOver={(e) => e.target.style.transform = 'scale(1.05)'}
+                  onMouseOut={(e) => e.target.style.transform = 'scale(1)'}
                 >
                   Toggle to {isCelsius ? "Fahrenheit" : "Celsius"}
                 </Button>
                 <Button
                   variant="contained"
                   color="primary"
+                  fullWidth
                   onClick={() => toggleFavorite(city.LocalizedName)}
-                  style={{ minWidth: '190px', textTransform: 'none' }}
+                  style={{ 
+                    textTransform: 'none', 
+                    background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+                    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)'
+                  }}
+                  onMouseOver={(e) => e.target.style.transform = 'scale(1.05)'}
+                  onMouseOut={(e) => e.target.style.transform = 'scale(1)'}
                 >
                   {isFavorite(city.Key) ? "Remove from Favorites" : "Add to Favorites"}
                 </Button>
               </div>
-              <Box height="40px" display="flex" alignItems="center">
-                {isFavorite(city.Key) && <Typography variant="h2" style={{ marginBottom: '50px', marginRight: '55px' }}>🌟</Typography>}
-              </Box>
             </Grid>
           </Grid>
           <Divider style={{ margin: '20px 0' }} />
-          <Grid container spacing={3} justifyContent="space-between">
+          <Grid container spacing={0} justifyContent="space-between">
             {weatherData.forecast.DailyForecasts.map((day, index) => (
               <Grid item xs key={index} style={{ textAlign: 'center', border: '1px solid black', padding: '10px', margin: '0 5px' }}>
                 <Typography variant="body1">{index === 0 ? 'Today' : getDayOfWeek(day.Date)}</Typography>
